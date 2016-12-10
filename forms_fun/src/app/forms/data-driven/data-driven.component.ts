32ba02c94d92483efa958182dfdb7ba4d7435fe1
Rafa³ Kucharski <r.kucharski@lgbs.pl>
2016-12-10 23:38:06 +0100

    Look at ng2 dynamic form arrays

--- forms_fun/src/app/forms/data-driven/data-driven.component.ts
+++ forms_fun/src/app/forms/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators, FormArray, AbstractControl} from "@angular/forms";
 
 @Component({
   selector: 'fm-data-driven',
@@ -15,6 +15,14 @@ export class DataDrivenComponent implements OnInit {
   submit() {
     console.log('submit', this.userForm);
   }
+
+  addHobby(){
+    let hobbies = <FormArray>this.userForm.controls['hobbies'];
+    // if(hobbies.controls[hobbies.length - 1].value) {
+      hobbies.push(new FormControl('', Validators.required))
+    // }
+  }
+
   constructor() {
     this.userForm = new FormGroup({
       userData: new FormGroup({
@@ -22,7 +30,10 @@ export class DataDrivenComponent implements OnInit {
         ['email'] : new FormControl('', [Validators.required, Validators.pattern('[^@ ]+@[^. ]+\.[a-z]{2,3}')]),
       }),
       'password': new FormControl('', [Validators.required]), // mozna uzywac stringow ale nie trzeba
-      'gender': new FormControl('chłopiec')
+      'gender': new FormControl('chłopiec'),
+       hobbies: new FormArray([
+         new FormControl('Cokaine', Validators.required)
+       ])
     });
   }
 
