c03cf61b2f8877a8268920ec811c690df9e319f7
Rafa³ Kucharski <r.kucharski@lgbs.pl>
2016-12-11 01:49:27 +0100

    Poking around with forms validation and custom validators in ng2

--- forms_fun/src/app/forms/data-driven/data-driven.component.ts
+++ forms_fun/src/app/forms/data-driven/data-driven.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators, FormArray, AbstractControl} from "@angular/forms";
+import {FormControl, FormGroup, Validators, FormArray, AbstractControl, FormBuilder} from "@angular/forms";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'fm-data-driven',
   templateUrl: './data-driven.component.html',
-  styleUrls: ['./data-driven.component.css']
+  styleUrls: ['./data-driven.component.css'],
 })
 export class DataDrivenComponent implements OnInit {
+  oldUserForm: FormGroup;
   userForm : FormGroup;
   genders = [
     'chłopiec',
@@ -23,8 +25,25 @@ export class DataDrivenComponent implements OnInit {
     // }
   }
 
-  constructor() {
-    this.userForm = new FormGroup({
+  // sprawdza obecnosc slowa test - test sprawi ze bedzie blad
+  customValidator(control: FormControl) : {[s: string] : boolean} {// zwraca obiekt posiadajacy klucz string bedacy boolem
+    if(control.value == "Test") {
+      return {example: true}; // jesli zwroci sie cokolwiek innego niz null lub undefined to validacja sie nie powiedzie
+    }
+
+  }
+
+  asyncCustomValidator(control: FormControl) : any {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log(control.value);
+        control.value == "Haszysz" ? resolve({dupa: true}) : resolve(null); // orpocz zwracania promisa dziala DOKLANDIE tak samo
+        // obecnsc slowa spowoduje blad - null/undefined - to poprawna walidacja
+      }, 1000);
+    })
+  }
+  constructor(private formBuilder: FormBuilder) {
+    this.oldUserForm = new FormGroup({
       userData: new FormGroup({
         username: new FormControl('Rafał', [Validators.required]), // wartosc, walidator
         ['email'] : new FormControl('', [Validators.required, Validators.pattern('[^@ ]+@[^. ]+\.[a-z]{2,3}')]),
@@ -35,9 +54,24 @@ export class DataDrivenComponent implements OnInit {
          new FormControl('Cokaine', Validators.required)
        ])
     });
+
+    // znacznie lepszy sposob
+    this.userForm = formBuilder.group({
+      userData: formBuilder.group({
+        username: ['Rafał', [Validators.required, this.customValidator]], // mozna przekazac zamiast obiektu FormCOntrol tablice
+        ['email'] : ['', [Validators.required, Validators.pattern('[^@ ]+@[^. ]+\.[a-z]{2,3}')]],
+      }),
+      'password': ['', [Validators.required]],
+      'gender': ['chłopiec'],
+      hobbies: formBuilder.array([
+        new FormControl('Cokaine', [Validators.required, this.asyncCustomValidator])
+      ])
+    });
   }
 
   ngOnInit() {
+    this.userForm.valueChanges.subscribe(x => console.log('values change',x));
+    this.userForm.statusChanges.subscribe(x => console.log('status change',x));
   }
 
 }
