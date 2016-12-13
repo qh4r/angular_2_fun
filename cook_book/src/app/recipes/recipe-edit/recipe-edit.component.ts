a253aeceaceac6446097d11ed7bbf37c693aef01
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-12-13 22:19:52 +0100

    Added recipe form to the cook book

--- cook_book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ cook_book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {RecipesService} from "../recipes.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'cb-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private recipesSubscription: Subscription;
+  private paramsSubscription: Subscription;
+  private selectedRecipeIndex: number;
+  private isNew: boolean;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipesService) {
+  }
 
   ngOnInit() {
+    this.isNew = true;
+    this.paramsSubscription = this.route.params.subscribe(x => {
+      this.selectedRecipeIndex = +x['id'];
+      if (x.hasOwnProperty('id')) {
+        this.isNew = false;
+      } else {
+        this.isNew = true;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
