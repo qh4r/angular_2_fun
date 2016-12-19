2d321bd002a2e75fa2f50ffd6cdea68490a93d6d
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-12-19 02:17:52 +0100

    Some fun with ng2 and firebase

--- playground/src/app/http/http.component.ts
+++ playground/src/app/http/http.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {HttpService} from "./http.service";
-import {Subscription} from "rxjs";
+import {Subscription, Observable} from "rxjs";
 import {Response} from "@angular/http";
+import {User} from "./user";
 
 @Component({
   selector: 'pg-http',
@@ -11,19 +12,42 @@ import {Response} from "@angular/http";
 })
 export class HttpComponent implements OnInit, OnDestroy {
   private urlSubscription: Subscription;
+  private postSubscription: Subscription;
+  private userDataSubscription: Subscription;
+  private userData: Array<User> = [];
 
   constructor(private httpService: HttpService) {
   }
 
   test = '';
+  asyncTest : Observable<any> | Promise<any> = null;
+
+  onFetch(){
+    this.asyncTest = this.httpService.getData();
+  }
 
   ngOnInit() {
     this.urlSubscription = this.httpService
-      .getData().subscribe((x: Response) => this.test = x.text());
+      .getData().subscribe((x: any) => this.test = x);
+  }
+
+  onSubmit(username: string, email: string) {
+    this.postSubscription = this.httpService.sendUserData(new User(username, email))
+      .subscribe(console.log);
+  }
+
+  onGetUsersData() {
+    this.userDataSubscription = this.httpService.getUserData()
+      .subscribe(x => {
+        this.userData = x;
+        console.log(x);
+      });
   }
 
   ngOnDestroy(): void {
     this.urlSubscription.unsubscribe();
+    this.postSubscription.unsubscribe();
+    this.userDataSubscription.unsubscribe();
   }
 
 }
--- playground/src/app/http/http.service.ts
+++ playground/src/app/http/http.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import {Http} from "@angular/http";
+import {Http, Response, Headers} from "@angular/http";
+import 'rxjs/Rx';
+import {User} from './user'
+import {Observable} from "rxjs";
+
 @Injectable()
 export class HttpService {
 
@@ -7,5 +11,32 @@ export class HttpService {
 
   getData(){
     return this.http.get('https://ng2-fun.firebaseio.com/test.json')
+      .map((response: Response) => response.json());
+  }
+
+  sendUserData(user: User){
+    const body = JSON.stringify(user);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    // headers.append(...) // tez wchodzi w gre
+    return this.http.post('https://ng2-fun.firebaseio.com/data.json', body, {
+      headers: headers
+    }).map((data: Response) => {
+      return data.json();
+    })
+      /// tak mozna handlowac bledy
+      .catch(x => {
+        console.error('oj oj blad: ', x);
+        return Observable.throw(x); // przerzucenie
+      });
+  }
+
+  getUserData() {
+    // .json mowi firebasowi ze ma zwrocic json i snapshota!!
+    return this.http.get('https://ng2-fun.firebaseio.com/data.json')
+      .map((response: Response) => ((data) =>
+        Object.keys(data).map(x => data[x])
+      )(response.json()));
   }
 }
